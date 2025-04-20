149814f52e0c00eb4726674a5372a8a4ab8348dc
Blooossom <taegyeong19@gmail.com>
2025-04-21 01:48:23 +0900

    feat: 업데이트 내역 App, main.tsx반영

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Header from "@/layout/Header.tsx";
+import Side from "@/layout/Side.tsx";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar.tsx";
+import { useState } from "react";
+import { cn } from "@/lib/utils.ts";
+import { clsx } from "clsx";
+import Main from "@/layout/Main.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [open, setOpen] = useState<boolean>(false);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={cn(`grid`, open ? `grid-cols-[256px,1fr]` : `grid-cols-[56px,1fr]`)}>
+      <Header open={open} />
+      <SidebarProvider open={open} onOpenChange={setOpen}>
+        <SidebarTrigger
+          className={clsx(
+            cn(`absolute w-11 h-11 mt-3 items-center justify-center`),
+            open ? `left-64` : `left-0`,
+          )}
+          size={'default'}
+        />
+        <Side />
+      </SidebarProvider>
+      <Main open={open} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import "./index.css";
+import "./assets/reset.css";
+import App from "./App.tsx";
+import {Toaster} from "./components/ui/sonner.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+      <App />
+      <Toaster />
   </StrictMode>,
-)
+);
